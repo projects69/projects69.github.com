89f173d9ad3314f49fbb28e26ad3afdc290d9690
magiczoho <magichefer@zohomail.com>
2022-11-28 19:31:10 +0700

    load clip and show data

---
+++ assets/js/data.js
@@ -0,0 +1,81 @@
+var dataClips = {
+	1: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	2: {
+		'urls': [
+			'https://mega.nz/embed/jdoTzDKa#G5rnaVBTd8JA0-ERvX9AqXvCS1c1nSuwvtxIhI123J8',
+			'https://mega.nz/embed/zABi2aSa#oZSqXHkig1RPAIdILfY2dKopGgNQsdTbRiieaxuqHhA',
+			'https://mega.nz/embed/6cBV3aaT#VclqLAUfVZPLbpkOkEE5l1ICt-Au5UuQnjPGvL5MofU',
+			'https://mega.nz/embed/7URTGaBL#Ip36ashOpns3hNVExMQ0Zqca-uEpULNNsiRthEOYFFY',
+			'https://mega.nz/embed/aBQ3UR6B#N8-k0-SawRsQbz1Bl8LI0uStLMXXFD_ITuHHjAU-Sg4',
+			'https://mega.nz/embed/ic4VWIhK#YqyVycGhgOWyo2rvJyXg8yTYXy1bBaXv_U77BIhjDmc',
+			'https://mega.nz/embed/vZAHFaoS#VQz5dFG5gnY3Ub22n-zXr4UzBJSctsakEkDyJptWHwE',
+			'https://mega.nz/embed/yAIV2CgY#nPYbkN8agAboSKv6QoEAgKhPZfmmGDsNbuvYu2C8Uig'
+		],
+		'tags': 'jav, lên đỉnh, rên la, móc lồn, quan hệ, doggy'
+	},
+	3: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	4: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	5: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	6: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	7: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	8: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	9: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	10: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	11: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	},
+	12: {
+		'urls': [
+			'https://mega.nz/embed/fJpQECBJ#dSH8_P2hEo7sihK161crWj1vwKzw4hp7JWW98048meg'
+		],
+		'tags': 'lên đỉnh, co giật, rên la, quan hệ'
+	}
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,119 @@ function includeHTML() {
   }
 }
 
+function loadItemsHome(page) {
+    var offset = 0;
+    var numItemInPage = 15;
+    if(page > 1) {
+        offset = numItemInPage * (page - 1);
+    }
+    var html = '';
+    var length = Object.keys(dataClips).length;
+    for (var id = (length - offset); id >= ((length - offset)-numItemInPage+1); id--) {
+        html += `<div class="item row border-bottom border-dotted my-4">
+                        <div class="col-4 px-0">
+                            <a class="d-block text-primary text-decoration-none" href="/?clip=${id}" target="_blank">Clip ${id}</a>
+                        </div>
+                        <div class="col-8 text-end px-0 text-nowrap overflow-hidden">${dataClips[id].tags}</div>
+                    </div>`;
+    }
+    $('.items').html(html);
+    if (Object.keys(dataClips).length > numItemInPage) {
+        var html = '';
+        for (var i = 1; i <= Math.ceil(Object.keys(dataClips).length / numItemInPage); i++) {
+            if(i == page) {
+                html += `<li class="page-item active" aria-current="page">
+                          <span class="page-link">${i}</span>
+                        </li>`;
+            } else {
+                html += `<li class="page-item"><a class="page-link" href="?page=${i}">${i}</a></li>`;
+            }
+        }
+        $('.pagination').html(html);
+    }
+}
+
+function loadItemClip(id) {
+    var clip = dataClips[id];
+    var html = '';
+    html += `<h1 class="fs-3">Clip sex ${id}</h1>`;
+    var i = 1;
+    for (var index in dataClips[id].urls) {
+        html += `<div class="item mt-4 mb-5">
+                        <iframe width="100%" height="360" frameborder="0" src="${dataClips[id].urls[index]}" allowfullscreen ></iframe>
+                        <p class="fw-bold text-center">Phần ${i}</p>
+                    </div>`;
+        i++;
+    }
+    html += `<div class="pt-0 border-bottom"><h2 class="fs-3">Bạn Muốn Xem Tiếp?</h2></div>`;
+    html += `<div class="px-3">`;
+    var indexed = 0;
+    var html2 = '';
+    for (var i = 1; i <= 3; i++) {
+        var index = id + i;
+        if(dataClips[index]) {
+            indexed += 1;
+            html2 += `<div class="item row border-bottom border-dotted my-3">
+                        <div class="col-4 px-0">
+                            <a class="d-block text-primary text-decoration-none" href="/?clip=${index}">Clip ${index}</a>
+                        </div>
+                        <div class="col-8 text-end px-0 text-nowrap overflow-hidden">${dataClips[index].tags}</div>
+                    </div>`;
+        }
+    }
+
+    if (indexed < 6) {
+        for (var i = 1; i <= (6-indexed); i++) {
+            var index = id - i;
+            if(dataClips[index]) {
+                html2 += `<div class="item row border-bottom border-dotted my-4">
+                            <div class="col-4 px-0">
+                                <a class="d-block text-primary text-decoration-none" href="/?clip=${index}">Clip ${index}</a>
+                            </div>
+                            <div class="col-8 text-end px-0 text-nowrap overflow-hidden">${dataClips[index].tags}</div>
+                        </div>`;
+            }
+        }
+    }
+    html += html2;
+
+    html += `</div>`;
+
+    html += `<div class="pt-0 border-bottom"><h2 class="fs-3">Clip Sex Mới Nhất</h2></div>`;
+    html += `<div class="px-3">`;
+    var length = Object.keys(dataClips).length;
+    for (var i = length; i >= (length - 5); i--) {
+        if(dataClips[i]) {
+            html += `<div class="item row border-bottom border-dotted my-3">
+                        <div class="col-4 px-0">
+                            <a class="d-block text-primary text-decoration-none" href="/?clip=${i}">Clip ${i}</a>
+                        </div>
+                        <div class="col-8 text-end px-0 text-nowrap overflow-hidden">${dataClips[i].tags}</div>
+                    </div>`;
+        }
+    }
+    html += `</div>`;
+    $('.items').html(html);
+}
+
 $( document ).ready(function() {
     includeHTML();
+    let searchParams = new URLSearchParams(window.location.search);
+    var page = 1;
+
+    if (searchParams.has('page')) {
+        if(parseInt(searchParams.get('page')) > 0) {
+            page = parseInt(searchParams.get('page'));
+        }
+    }
+
+    if (searchParams.has('clip')) {
+        if(parseInt(searchParams.get('clip')) > 0) {
+            loadItemClip(parseInt(searchParams.get('clip')));
+        } else {
+            loadItemsHome(page);
+        }
+    } else {
+        loadItemsHome(page);
+    }
 });
